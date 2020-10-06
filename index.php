7eb546eac8edc60532468382ebf216f8bbfd5e51
Nathan Zweifel <nathan.zweifel@heig-vd.ch>
2020-10-06 18:24:35 +0200

    Changed password to match with the database password

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 define('BASE_URL', '/');
 define('DB_USER', 'todolist');
-define('DB_PASS', 'chAngeMeN0w!');
+define('DB_PASS', 'chAngeMeN0w');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
