8ce548360cfb03f95c4afbe0ca5a07c1dfc7697a
Robin Zweifel <robin.zweifel@gmail.com>
2020-12-11 13:46:41 +0100

    Title changed

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ $items = $db->query($selectQuery);
       <div class="navbar navbar-dark bg-dark shadow-sm">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <strong>TodoList</strong>
+            <strong>Hooked TodoList</strong>
           </a>
         </div>
       </div>
